f2c98ee9cfaf78c7af4604f8f6f84be767a5fa10
Eltun <aslanoveltun28@gmail.com>
2025-03-02 12:36:00 +0400

    init commit

---
+++ src/main/java/org/ea/cinevibe/CinevibeApplication.java
@@ -0,0 +1,13 @@
+package org.ea.cinevibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinevibeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinevibeApplication.class, args);
+    }
+
+}
